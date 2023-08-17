44ca3bece86b5e2c58a0071ab2a73afffa3ae7a8
vladislav <chasinvlad39@gmail.com>
2023-08-17 23:02:55 +0300

    learning block ready!

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,9 +1,11 @@
 import FlexMethod from "./flexMetod.js"
+import Learning from "./learning.js"
 
 export default function Main(){
     return(
         <main>
             <FlexMethod/>
+            <Learning/>
         </main>
     )
 }
